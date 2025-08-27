ba99692d339551bdaabb269e7fe414f26b580d64
Thalya Steffany <thalyacoiimbra@gmail.com>
2025-08-27 07:41:54 -0300

    chore: configure nunito and inter fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,18 @@
-import { useFonts } from "expo-font";
+import {
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold_Italic,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+  useFonts as useFontsInter,
+} from "@expo-google-fonts/inter";
+import {
+  Nunito_400Regular,
+  Nunito_500Medium,
+  Nunito_600SemiBold,
+  Nunito_700Bold,
+  useFonts as useFontsNunito,
+} from "@expo-google-fonts/nunito";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
@@ -8,9 +22,22 @@ import "./styles/global.css";
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [fontsLoaded] = useFonts({
-    "SpaceMono-Regular": require("@/assets/fonts/SpaceMono-Regular.ttf"),
+  const [nunitoLoaded] = useFontsNunito({
+    Nunito_400Regular,
+    Nunito_500Medium,
+    Nunito_600SemiBold,
+    Nunito_700Bold,
   });
+
+  const [interLoaded] = useFontsInter({
+    Inter_400Regular,
+    Inter_500Medium,
+    Inter_600SemiBold_Italic,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  const fontsLoaded = nunitoLoaded && interLoaded;
 
   useEffect(() => {
     if (fontsLoaded) {
@@ -27,33 +54,34 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen
-        name="index"
+        name='index'
         options={{
           title: "Bem-vindo",
-          headerShown: false
+          headerShown: false,
         }}
       />
       <Stack.Screen
-        name="login"
+        name='login'
         options={{
           title: "Login",
-          headerShown: true
+          headerShown: true,
         }}
       />
       <Stack.Screen
-        name="home"
+        name='home'
         options={{
           title: "Home",
-          headerShown: true
+          headerShown: true,
         }}
       />
       <Stack.Screen
-        name="transactions"
+        name='transactions'
         options={{
           title: "Transações",
-          headerShown: false
+          headerShown: false,
         }}
       />
     </Stack>
   );
 }
+
