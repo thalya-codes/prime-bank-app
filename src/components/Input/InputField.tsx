3b8f8f8fb19220cd32e8427ca012718fce7a7115
Thalya Steffany <thalyacoiimbra@gmail.com>
2025-08-28 19:47:05 -0300

    feat: create Input component

---
+++ src/components/Input/InputField.tsx
@@ -0,0 +1,11 @@
+import { TextInput, TextInputProps } from "react-native";
+
+export const InputField = (props: TextInputProps) => {
+  return (
+    <TextInput
+      className='flex-1 text-neutral-700 placeholder placeholder-neutral-400'
+      {...props}
+    />
+  );
+};
+
