2d5531acf241ca5ec93424f4bd743fc223a95183
Thalya Steffany <thalyacoiimbra@gmail.com>
2025-09-16 17:01:15 -0300

    fix(Checkbox): fix font size

--- src/components/Checkbox/CheckboxLabel.tsx
+++ src/components/Checkbox/CheckboxLabel.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { Text } from "react-native";
 
 export const CheckboxLabel = ({ children }: { children: ReactNode }) => (
-  <Text className='font-inter-semi-bold text-md text-neutral-900'>
+  <Text className='font-inter-semi-bold text-base text-neutral-900'>
     {children}
   </Text>
 );
