275f85edb65b154e6da27bc03c61b1cd08d7dae9
Thalya Steffany <thalyacoiimbra@gmail.com>
2025-09-16 17:21:21 -0300

    feat: create Login screen

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -3,8 +3,9 @@ import { View } from "react-native";
 
 export default function LoginScreen() {
   return (
-    <View className="flex-1">
+    <View className='flex-1 justify-center items-center bg-brand-100'>
       <LoginPage />
     </View>
   );
 }
+
