2ab199e4d32829d17673c78a84eb389657ca5772
Thalya Steffany <thalyacoiimbra@gmail.com>
2025-09-16 17:26:32 -0300

    feat: create ResetPassword screen

---
+++ src/app/reset-password.tsx
@@ -0,0 +1,11 @@
+import { ResetPasswordPage } from "@/pages/ResetPasswordPage";
+import { View } from "react-native";
+
+export default function ResetPasswordScreen() {
+  return (
+    <View className='flex-1 justify-center items-center bg-brand-100'>
+      <ResetPasswordPage />
+    </View>
+  );
+}
+
---
+++ src/pages/ResetPasswordPage/index.tsx
@@ -0,0 +1,54 @@
+import { Button } from "@/components/Button";
+import { FormFieldLabel } from "@/components/FormField/FormFieldLabel";
+import { FormFieldRoot } from "@/components/FormField/FormFieldRoot";
+import { InputPassword } from "@/components/Input";
+import { PublicScreenLayout } from "@/layouts/PublicScreenLayout";
+import { passwordAndConfirmPasswordSchema } from "@/utils/validations";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useRouter } from "expo-router";
+import { useForm } from "react-hook-form";
+
+export function ResetPasswordPage() {
+  const router = useRouter();
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    defaultValues: {
+      password: "",
+      confirmPassword: "",
+    },
+    mode: "onBlur",
+    resolver: yupResolver(passwordAndConfirmPasswordSchema),
+  });
+
+  const onrResetPassword = () => {
+    router.push("/login");
+  };
+
+  return (
+    <PublicScreenLayout title='Redefinir senha'>
+      <FormFieldRoot>
+        <FormFieldLabel>Nova senha</FormFieldLabel>
+        <InputPassword
+          control={control}
+          name='password'
+          error={errors?.password}
+        />
+      </FormFieldRoot>
+
+      <FormFieldRoot>
+        <FormFieldLabel>Confirmar nova senha</FormFieldLabel>
+        <InputPassword
+          control={control}
+          name='confirmPassword'
+          error={errors?.confirmPassword}
+        />
+      </FormFieldRoot>
+
+      <Button text='Alterar' className='gap-2' onPress={handleSubmit(onrResetPassword)} />
+    </PublicScreenLayout>
+  );
+}
+
