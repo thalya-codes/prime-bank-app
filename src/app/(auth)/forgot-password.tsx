68666022cacb3eba44d9ff75c11470cd2b12b01e
davimattos <davimattosdev@gmail.com>
2025-09-21 20:53:47 -0300

    refactor: adjusting routes

---
+++ src/app/(auth)/forgot-password.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+
+export default function CreatAccountScreen() {
+  return (
+    <View className='flex-1 justify-center items-center bg-brand-100'>
+      <></>
+    </View>
+  );
+}
+
