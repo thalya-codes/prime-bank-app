d8f39eb29246e20577c0329a3beca88b82dd931a
davimattos <davimattosdev@gmail.com>
2025-09-23 18:03:43 -0300

    refactor: fix the screen return into to forgot password page

--- src/app/(auth)/forgot-password.tsx
+++ src/app/(auth)/forgot-password.tsx
@@ -1,9 +1,10 @@
+import { ResetPasswordPage } from "@/pages";
 import { View } from "react-native";
 
-export default function CreatAccountScreen() {
+export default function ForgotPasswordScreen() {
   return (
     <View className='flex-1 justify-center items-center bg-brand-100'>
-      <></>
+      <ResetPasswordPage/>
     </View>
   );
 }
