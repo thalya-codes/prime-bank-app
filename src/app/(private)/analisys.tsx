56e81a2baed3b27aa8ef3bbb835c7ff51a2f0928
Lucas Siqueira <54068176+lucasvss2@users.noreply.github.com>
2025-09-30 17:02:48 -0300

    feat/Analysis page

---
+++ src/app/(private)/analisys.tsx
@@ -0,0 +1,12 @@
+
+import { AnalysisPage } from "@/pages";
+import { SafeAreaView } from "react-native";
+
+export default function AnalysisScreen() {
+  return (
+    <SafeAreaView className='flex-1'>
+      <AnalysisPage />
+    </SafeAreaView>
+  );
+}
+
