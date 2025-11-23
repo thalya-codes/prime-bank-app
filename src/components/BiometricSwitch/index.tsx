b4635c9ffc5641eb4df19e1da406ed42e9a49652
Thalya Steffany <thalyacoiimbra@gmail.com>
2025-11-22 22:09:46 -0300

    refactor(BiometricSwitch): remove uncessary useEffect

--- src/components/BiometricSwitch/index.tsx
+++ src/components/BiometricSwitch/index.tsx
@@ -1,27 +1,26 @@
-import { getBiometricPreference, saveBiometricPreference } from "@/utils/auth/secureStore";
-import { useEffect, useState } from "react";
+import { useBiometricAuth } from "@/hooks/useBiometricAuth";
+import { useBiometricAuthStore } from "@/store/useBiometricAuthStore";
+import { saveBiometricPreference } from "@/utils/auth/secureStore";
 import { Switch, View } from "react-native";
 import { FormFieldLabel } from "../FormField/FormFieldLabel";
 
 export const BiometricSwitch = () => {
-  const [enableBiometric, setEnableBiometric] = useState(false);
-  const toggleEnableBiometrics = () =>
-    setEnableBiometric((previousState) => !previousState);
+  const { enableBiometric, setEnableBiometric } = useBiometricAuthStore();
 
-  useEffect(() => {
-    const biometricPreference = getBiometricPreference();
-
-    if (biometricPreference) {
-      return setEnableBiometric(Boolean(biometricPreference));
-    } 
-
-    if(enableBiometric) saveBiometricPreference(enableBiometric)
-  }, [enableBiometric]);
+  const { isBiometricSetted } = useBiometricAuth();
+  const toggleEnableBiometrics = () => {
+    setEnableBiometric(!enableBiometric);
+    saveBiometricPreference(!enableBiometric);
+  };
 
   return (
     <View className='flex-row gap-3'>
       <FormFieldLabel>Habilitar biometria</FormFieldLabel>
-      <Switch value={enableBiometric} onChange={toggleEnableBiometrics} />
+      <Switch
+        value={enableBiometric}
+        onChange={toggleEnableBiometrics}
+        disabled={!isBiometricSetted}
+      />
     </View>
   );
 };
